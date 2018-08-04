2f9a46262ce2657c5fe7da06490648d84a77df8c
JacRoss <tori.belechy@gmail.com>
2018-08-05 00:08:36 +0300

    -

--- src/Sum.php
+++ src/Sum.php
@@ -5,7 +5,7 @@ namespace Jackross;
 class Sum
 {
 
-    public function sum(int $a, int $b): int
+    public function sum(int $a, int $b): string
     {
         return sprintf("Dmitry says, that %s + %s = ", $a, $b, $a + $b);
     }
